8983f0661dc6637a686e595b6eecbe547afa49f0
Justin Kahshen Lu <justinklu75@gmail.com>
2025-08-31 15:04:57 -0400

    refactor: update to pages to improve navigation

---
+++ src/content/_meta.js
@@ -0,0 +1,22 @@
+export default {
+    portfolio: {
+        title: 'Portfolio',
+        type: 'page'
+    },
+    projects: {
+        title: 'Projects',
+        type: 'page',
+    },
+    gallery: {
+        title: 'Gallery',
+        type: 'page',
+    },
+    index: {
+        display: 'hidden'
+    },
+    blog: {
+        title: 'Blog',
+        type: 'page',
+        
+    }
+}
