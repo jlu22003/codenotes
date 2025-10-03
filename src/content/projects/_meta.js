c170e975f96f1b9b179605483cc40018180efd4d
Justin Kahshen Lu <justinklu75@gmail.com>
2025-10-03 12:54:35 -0400

    Update: Projects section update

--- src/content/projects/_meta.js
+++ src/content/projects/_meta.js
@@ -3,8 +3,24 @@ export default {
         title: 'Work In Progress',
         type: 'doc'
     },
+    personalportfolio: {
+        title: 'Personal Portfolio',
+        type: 'doc'
+    },
     tictactoe: {
         title: 'Tic Tac Toe',
+        type: 'doc'
+    },
+    securechatapp: {
+        title: 'Secure Chat App',
+        type: 'doc'
+    },
+    raspberrypi: {
+        title: 'Raspberry Pi',
+        type: 'doc'
+    },
+    riftrewind: {
+        title: 'Rift Rewind',
         type: 'doc'
     },
 }
